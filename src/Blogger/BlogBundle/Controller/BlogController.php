9f3fe6d4503c093f5e1f761a5af7f907c2921711
Jok4 <jokin.zulaika@opendeusto.es>
2017-11-17 12:15:20 +0100

    nueva db y cambios varios

--- src/Blogger/BlogBundle/Controller/BlogController.php
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -31,5 +31,31 @@ class BlogController extends Controller
 	{
 		return $this->render('BloggerBlogBundle:Blog:contact.html.twig');
 	}
+
+	public function categoriaAction()
+	{
+		return $this->render('BloggerBlogBundle:Blog:categoria.html.twig');
+	}
+
+	public function entranteAction()
+	{
+		$posts = $this->get('doctrine')->getManager()->createQueryBuilder()->select('p')->from('BloggerBlogBundle:Post',  'p')->where('p.categoria = :entrante')->addOrderBy('p.categoria', 'ASC')->setParameter('entrante', 'Entrante')->getQuery()->getResult();
+
+		return $this->render('BloggerBlogBundle:Blog:entrante.html.twig', array('posts' => $posts));
+	}
+
+	public function principalAction()
+	{
+		$posts = $this->get('doctrine')->getManager()->createQueryBuilder()->select('p')->from('BloggerBlogBundle:Post',  'p')->where('p.categoria = :principal')->addOrderBy('p.categoria', 'ASC')->setParameter('principal', 'Principal')->getQuery()->getResult();
+
+		return $this->render('BloggerBlogBundle:Blog:principal.html.twig', array('posts' => $posts));
+	}
+
+	public function postreAction()
+	{
+		$posts = $this->get('doctrine')->getManager()->createQueryBuilder()->select('p')->from('BloggerBlogBundle:Post',  'p')->where('p.categoria = :postre')->addOrderBy('p.categoria', 'ASC')->setParameter('postre', 'Postre')->getQuery()->getResult();
+
+		return $this->render('BloggerBlogBundle:Blog:postre.html.twig', array('posts' => $posts));
+	}
 }
 ?>
